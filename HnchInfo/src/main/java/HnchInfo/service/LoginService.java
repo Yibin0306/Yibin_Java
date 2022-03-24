1e9542b1fe2bf12e44fa6635c6e69309b7fd3a93
伊斌 <972164359@qq.com>
2022-03-24 15:53:19 +0800

    更改

--- HnchInfo/src/main/java/HnchInfo/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package HnchInfo.service;
-
-import HnchInfo.entity.Employee;
-
-public interface LoginService {
-    //管理员登录功能
-    Employee login(Employee employee);
-}
--- HnchInfo/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello SpringBoot!</h2>
-</body>
-</html>
