b6af649a6539affe7b8308b9d7c46e4830500cc6
伊斌 <972164359@qq.com>
2022-03-28 11:03:12 +0800

    更改

--- ClassSystem/src/main/java/ClassSystem/entity/ServicePost.java
+++ ClassSystem/src/main/java/ClassSystem/entity/ServicePost.java
@@ -1,17 +1,54 @@
 package ClassSystem.entity;
 
-import org.springframework.stereotype.Component;
+import ClassSystem.utility.Const;
 
-@Component
-public class ServicePost {
+import java.io.Serializable;
+import java.util.List;
+
+public class ServicePost<R> implements Serializable {
+    private int code;
     private String msg;
-    private Integer code;
+    private R data;
+    private List<R> list;
 
     public ServicePost() {
     }
 
-    public ServicePost(String msg, Integer code) {
+    public ServicePost(int code, String msg) {
+        this.code = code;
         this.msg = msg;
+    }
+
+    public ServicePost(int code, R data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public ServicePost(int code, List<R> list) {
+        this.code = code;
+        this.list = list;
+    }
+
+    public ServicePost(int code, String msg, R data, List<R> list) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+        this.list = list;
+    }
+
+    public static ServicePost CreateTrueCodMsg(String msg){
+        return new ServicePost(Const.true_200,msg);
+    }
+
+    public static ServicePost CreateErrorCodMsg(String msg){
+        return new ServicePost(Const.false_101,msg);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
         this.code = code;
     }
 
@@ -23,19 +60,29 @@ public class ServicePost {
         this.msg = msg;
     }
 
-    public Integer getCode() {
-        return code;
+    public R getData() {
+        return data;
     }
 
-    public void setCode(Integer code) {
-        this.code = code;
+    public void setData(R data) {
+        this.data = data;
+    }
+
+    public List<R> getList() {
+        return list;
+    }
+
+    public void setList(List<R> list) {
+        this.list = list;
     }
 
     @Override
     public String toString() {
         return "ServicePost{" +
-                "msg='" + msg + '\'' +
-                ", code=" + code +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                ", list=" + list +
                 '}';
     }
 }
--- ClassSystem/src/main/java/ClassSystem/service/Impl/UserServiceImpl.java
+++ ClassSystem/src/main/java/ClassSystem/service/Impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import ClassSystem.entity.ServicePost;
 import ClassSystem.entity.User;
 import ClassSystem.mapper.UserMapper;
 import ClassSystem.service.UserService;
-import ClassSystem.utility.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +11,15 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
-    @Autowired
-    private ServicePost servicePost;
 
     //登录验证
     @Override
     public ServicePost login(User user) {
         User login = userMapper.login(user);
         if (login!=null){
-            servicePost.setMsg("欢迎您，娇贵的小公主~");
-            servicePost.setCode(Const.true_200);
+            return ServicePost.CreateTrueCodMsg("欢迎您，娇贵的小公主~");
         }else{
-            servicePost.setMsg("账号或密码错误呢~");
-            servicePost.setCode(Const.false_101);
+            return ServicePost.CreateErrorCodMsg("账号或密码错误呢~");
         }
-        return servicePost;
     }
 }
