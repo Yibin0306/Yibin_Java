f9acdcc7f5f8206512cccd17899b1a97eab08b46
伊斌 <972164359@qq.com>
2022-04-01 14:00:56 +0800

    更改

--- ClassSystem/src/main/java/ClassSystem/utility/RedisTemplateUtil.java
+++ ClassSystem/src/main/java/ClassSystem/utility/RedisTemplateUtil.java
@@ -3,7 +3,6 @@ package ClassSystem.utility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
 public class RedisTemplateUtil {
