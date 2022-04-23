73f5360361280214b705166ca6c4c7d3bb091317
伊斌 <972164359@qq.com>
2022-04-23 15:37:28 +0800

    更改

--- Reggie/src/main/java/Reggie/controller/EmployeeController.java
+++ Reggie/src/main/java/Reggie/controller/EmployeeController.java
@@ -3,14 +3,12 @@ package Reggie.controller;
 import Reggie.common.R;
 import Reggie.entity.Employee;
 import Reggie.service.EmployeeService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
@@ -30,11 +28,31 @@ public class EmployeeController {
         //1、将页面握交的密码password进行md5加密处理
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
+
         //2、根据页面提交的用户名username查询数据库
+        //包装成一个查询对象
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        //数据库内是索引唯一的用户名数据 所以调用getOne
+        Employee emp = employeeService.getOne(queryWrapper);
+
         //3、如果没有查询到则返回登录失败结果
+        if (emp == null){
+            return R.error("此用户没有注册呢~");
+        }
+
         //4、密码比对，如果不一致则返回登录失败结果
+        if (!emp.getPassword().equals(password)){
+            return R.error("密码错误呢~");
+        }
+
         //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if (emp.getStatus() == 0){
+            return R.error("用户账号被禁用了呢~");
+        }
+
         //6、登录成功，将员工id存入Session并返回登录成功结果
-        return null;
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
     }
 }
