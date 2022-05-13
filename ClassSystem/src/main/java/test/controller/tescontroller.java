0a64f39b80976986d928b3d6ea7679c656ef6375
yibin <972164359@qq.com>
2022-05-13 14:52:55 +0800

    更改

---
+++ ClassSystem/src/main/java/test/controller/tescontroller.java
@@ -0,0 +1,10 @@
+package test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class tescontroller {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+}
